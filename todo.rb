dc26af47fccdce64943e4754ca4c781a9a71fa05
Daniele Trovato <plasticsoup@yahoo.co.uk>
2020-01-22 16:52:33 +0000

    add lesson 6 create a new todo list

--- todo.rb
+++ todo.rb
@@ -1,15 +1,33 @@
 require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
+require "pry"
+
+configure do
+  enable :sessions
+  set :session_secret, 'secret'
+end
+
+before do
+  session[:lists] ||= []
+end
 
 get "/" do
   redirect "/lists"
 end
 
 get "/lists" do
-  @lists = [
-    {name: "Lunch Groceries", todos: []},
-    {name: "Dinner Groceries", todos: []}
-  ]
+  # binding.pry
+  @lists = session[:lists]
+
   erb :lists
+end
+
+get "/lists/new" do
+  erb :new_list
+end
+
+post "/lists" do
+  session[:lists] << { name: params[:list_name], todos: [] }
+  redirect "/lists"
 end
